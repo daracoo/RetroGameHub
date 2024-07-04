8ed745fc39022537c36c84fecb362191e350c10e
daracoo <dkuzmanoski21@gmail.com>
2024-07-05 00:14:14 +0200

    Added Minesweeper functionality

--- RetroGameHub/RetroGameHub/Minesweeper.Designer.cs
+++ RetroGameHub/RetroGameHub/Minesweeper.Designer.cs
@@ -28,24 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Minesweeper));
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.lblScore = new System.Windows.Forms.Label();
             this.lbScore = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Location = new System.Drawing.Point(35, 37);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(351, 337);
+            this.panel1.Size = new System.Drawing.Size(400, 400);
             this.panel1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(245, 387);
+            this.label1.Location = new System.Drawing.Point(302, 451);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(51, 19);
             this.label1.TabIndex = 1;
@@ -64,23 +68,32 @@
             // 
             this.lbScore.AutoSize = true;
             this.lbScore.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbScore.Location = new System.Drawing.Point(302, 387);
+            this.lbScore.Location = new System.Drawing.Point(359, 451);
             this.lbScore.Name = "lbScore";
             this.lbScore.Size = new System.Drawing.Size(18, 19);
             this.lbScore.TabIndex = 3;
             this.lbScore.Text = "0";
             // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "bomb.png");
+            this.imageList1.Images.SetKeyName(1, "dollar.jpg");
+            this.imageList1.Images.SetKeyName(2, "tile.png");
+            // 
             // Minesweeper
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(424, 466);
+            this.ClientSize = new System.Drawing.Size(477, 498);
             this.Controls.Add(this.lbScore);
             this.Controls.Add(this.lblScore);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Name = "Minesweeper";
             this.Text = "Minesweeper";
+            this.Load += new System.EventHandler(this.Minesweeper_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,5 +105,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblScore;
         private System.Windows.Forms.Label lbScore;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
--- RetroGameHub/RetroGameHub/Minesweeper.cs
+++ RetroGameHub/RetroGameHub/Minesweeper.cs
@@ -12,9 +12,92 @@ namespace RetroGameHub
 {
     public partial class Minesweeper : Form
     {
+        int score = 0;
+        PictureBox[] tiles = new PictureBox[100];
         public Minesweeper()
         {
             InitializeComponent();
+        }
+
+        private void Minesweeper_Load(object sender, EventArgs e)
+        {
+            load_tiles();
+        }
+
+        private void load_tiles()
+        {
+            int x = 0;
+            int y = 0;
+            int cnt = 0;
+            Random random = new Random();
+
+            for(int i = 0; i< 100; i++)
+            {
+                int num = random.Next(0, 2);
+                tiles[i] = new PictureBox();
+                tiles[i].Height = 40;
+                tiles[i].Width = 40;
+                tiles[i].Left = x;
+                tiles[i].Top = y;
+                tiles[i].BorderStyle = BorderStyle.FixedSingle;
+                tiles[i].Image = imageList1.Images[2];
+                if (num == 0)
+                {
+                    tiles[i].Tag = "Bomb";
+                }
+                else
+                {
+                    tiles[i].Tag = "Dollar";
+                }
+                tiles[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                tiles[i].Click += new System.EventHandler(ClickTile);
+                panel1.Controls.Add(tiles[i]);
+                x = x + 40;
+                cnt++;
+                if (cnt == 10)
+                {
+                    x = 0;
+                    y = y + 40;
+                    cnt = 0;
+                }
+            }
+        }
+
+        private void ClickTile(Object sender, System.EventArgs e)
+        {
+            DialogResult dg;
+            PictureBox tile2 = (PictureBox)sender;
+
+            if(tile2.Tag.ToString() != "Clicked")
+            {
+                if (tile2.Tag.ToString() == "Bomb")
+                {
+                    tile2.Image = imageList1.Images[0];
+                    dg = MessageBox.Show("Game Over!, Your Score: " + score.ToString() + "\nPlay Again?", "Game Over", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (dg == DialogResult.Yes)
+                    {
+                        score = 0;
+                        lbScore.Text = "0";
+                        foreach (PictureBox tile in tiles)
+                        {
+                            panel1.Controls.Remove(tile);
+                        }
+                        load_tiles();
+                    }
+                    else
+                    {
+                        Application.Exit();
+                    }
+                }
+                else if (tile2.Tag.ToString() == "Dollar")
+                {
+                    tile2.Image = imageList1.Images[1];
+                    tile2.Tag = "Clicked";
+                    score += 5;
+                    lbScore.Text = score.ToString();
+                }
+            }
+
         }
     }
 }
