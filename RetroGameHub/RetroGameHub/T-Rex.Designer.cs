f6080f5702b4aec9c6fa2a41402cc336ce6298cb
daracoo <dkuzmanoski21@gmail.com>
2024-07-06 20:34:17 +0200

    Added some T-Rex functionality

---
+++ RetroGameHub/RetroGameHub/T-Rex.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace RetroGameHub
+{
+    partial class T_Rex
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tRex = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tRex)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(-15, 334);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(749, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tRex
+            // 
+            this.tRex.Image = global::RetroGameHub.Properties.Resources.running;
+            this.tRex.Location = new System.Drawing.Point(131, 292);
+            this.tRex.Name = "tRex";
+            this.tRex.Size = new System.Drawing.Size(40, 43);
+            this.tRex.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.tRex.TabIndex = 1;
+            this.tRex.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::RetroGameHub.Properties.Resources.obstacle_1;
+            this.pictureBox3.Location = new System.Drawing.Point(504, 288);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(23, 46);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "obstacle";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::RetroGameHub.Properties.Resources.obstacle_2;
+            this.pictureBox4.Location = new System.Drawing.Point(588, 301);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(32, 33);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 1;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "obstacle";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(21, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(90, 27);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "Score: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.MainGameTimerEvent);
+            // 
+            // T_Rex
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 377);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.tRex);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "T_Rex";
+            this.Text = "T-Rex";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.T_Rex_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.T_Rex_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tRex)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox tRex;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
---
+++ RetroGameHub/RetroGameHub/T-Rex.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RetroGameHub
+{
+    public partial class T_Rex : Form
+    {
+        bool jumping = false;
+        int jumpSpeed = 12;
+        int force = 12;
+        int score = 0;
+        int obstacleSpeed = 10;
+        Random random = new Random();
+        int position;
+        bool isGameOver = false;
+
+        public T_Rex()
+        {
+            InitializeComponent();
+            GameReset();
+        }
+
+        private void MainGameTimerEvent(object sender, EventArgs e)
+        {
+
+        }
+
+        private void T_Rex_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void T_Rex_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void GameReset()
+        {
+            force = 12;
+        }
+    }
+}
